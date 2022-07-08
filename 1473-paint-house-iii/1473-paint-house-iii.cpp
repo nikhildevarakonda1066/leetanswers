163356653b2a187b98d959f0922871be1c84b0c3
nikhildevarakonda1066 <94293600+nikhildevarakonda1066@users.noreply.github.com>
2022-07-08 09:12:41 +0530

    Time: 170 ms (35.64%), Space: 19.4 MB (9.04%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+ public:
+  int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n,
+              int target) {
+    dp.resize(target + 1, vector<vector<int>>(m, vector<int>(n + 1)));
+    const int c = minCost(houses, cost, m, n, target, 0, 0);
+    return c == kMax ? -1 : c;
+  }
+
+ private:
+  constexpr static int kMax = 1e6 + 1;
+  vector<vector<vector<int>>> dp;
+
+  int minCost(const vector<int>& houses, const vector<vector<int>>& cost,
+              const int& m, const int& n, int k, int i, int prevColor) {
+    if (i == m || k < 0)
+      return k == 0 ? 0 : kMax;
+    if (dp[k][i][prevColor] > 0)
+      return dp[k][i][prevColor];
+    if (houses[i] > 0)  
+      return minCost(houses, cost, m, n, k - (prevColor != houses[i]), i + 1,
+                     houses[i]);
+    int ans = kMax;
+    for (int color = 1; color <= n; ++color)
+      ans = min(ans, cost[i][color - 1] + minCost(houses, cost, m, n,
+                                                  k - (prevColor != color),
+                                                  i + 1, color));
+    return dp[k][i][prevColor] = ans;
+  }
+};
